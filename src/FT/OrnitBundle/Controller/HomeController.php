244a9a0009c4a093202ef209b860e6199663e66e
DIJOUX <emmanuel.dijoux14@gmail.com>
2017-05-21 11:32:59 +0200

    intégration de la map

--- src/FT/OrnitBundle/Controller/HomeController.php
+++ src/FT/OrnitBundle/Controller/HomeController.php
@@ -3,16 +3,20 @@
 namespace FT\OrnitBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use Symfony\Component\HttpFoundation\Response;
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+
+
 
 class HomeController extends Controller
 {
     public function indexAction()
     {
+
+    	
+
         return $this->render('FTOrnitBundle:Home:index.html.twig');
     }
 }
