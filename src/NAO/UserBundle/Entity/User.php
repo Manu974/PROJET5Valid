6b467f549fe3be00fb9fe19127800b0f563c0892
psi974 <psi974479@gmail.com>
2017-06-06 11:42:34 +0400

    Création Bundle User+FOSUser + BDD + Paramétrages

---
+++ src/NAO/UserBundle/Entity/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace NAO\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="NAO\UserBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
+
