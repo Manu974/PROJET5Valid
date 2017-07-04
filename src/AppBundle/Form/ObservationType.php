63f63de24b4579764f2206e7c48146aec008e760
Manu974 <emmanuel.dijoux14@gmail.com>
2017-07-04 17:23:09 +0200

    création du formulaire pour l'observation

---
+++ src/AppBundle/Form/ObservationType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ObservationType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('location')->add('comment')->add('isValid')->add('createdAt')->add('author')->add('department')->add('nombreOiseaux')->add('maturite')->add('nidification')->add('bird')->add('image');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Observation'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_observation';
+    }
+
+
+}
