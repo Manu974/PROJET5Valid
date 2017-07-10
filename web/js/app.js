d1e3ea943780a1ec935c642ce64b84ed1655bd76
Arnoult marc <marc.arnoult76@gmail.com>
2017-07-10 14:49:17 +0200

    Initialisation de la page login en ajax

--- web/js/app.js
+++ web/js/app.js
@@ -3,3 +3,8 @@
 let menuBurger = new MenuSlider('.icon.icon--burger', '.navbar-mobile-content', '305px');
 
 new HeaderShrink('.header', 200, '88px', menuBurger);
+
+
+new loadPage('#app', '/login');
+//let loginSlider = new MenuSlider('.icon--connexion', '.');
+
