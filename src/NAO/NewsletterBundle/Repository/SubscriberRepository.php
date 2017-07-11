d088c84013171adc366bdcd15ca0dd648064c764
Manu974 <emmanuel.dijoux14@gmail.com>
2017-07-11 19:32:51 +0200

    ajout de la newsletter

---
+++ src/NAO/NewsletterBundle/Repository/SubscriberRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace NAO\NewsletterBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * SubscriberRepository
+ *
+ */
+class SubscriberRepository extends EntityRepository
+{
+
+}
