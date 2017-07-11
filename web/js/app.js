54f0b2e6a05d605cc7e918cecdfdd270af795483
Imac-marc <marc.arnoult@hotmail.fr>
2017-07-11 11:55:03 +0200

    finalisation du slider de connection

--- web/js/app.js
+++ web/js/app.js
@@ -3,8 +3,5 @@
 let menuBurger = new MenuSlider('.icon.icon--burger', '.navbar-mobile-content', '305px');
 
 new HeaderShrink('.header', 200, '88px', menuBurger);
-
-
-new loadPage('#app', '/login');
-//let loginSlider = new MenuSlider('.icon--connexion', '.');
+new loadLoginPage('#login-page', '/login', '.icon--connection');
 
