36d6aedfefa0d4c249978eb63166e7559b04e720
Imac-marc <marc.arnoult@hotmail.fr>
2017-07-13 17:10:09 +0200

    Finalisation des pages lié à l'administration et aux changements de couleur lié avec

---
+++ web/js/src/admin/admin-pages.js
@@ -0,0 +1,29 @@
+(function () {
+    let adminColor = '#00A99D';
+
+    let navbarMobile = document.querySelector('.navbar-mobile-content');
+    let links = document.querySelectorAll('section a:not(.btn), .article-footer a');
+    let buttons = document.querySelectorAll('section .btn:not(.btn--large), button');
+    let icons = document.querySelectorAll('.navbar-mobile-content .icon, section .icon, section .icon--large, .comments .icon, .blog-title .icon, .observation-container .icon, .observation-container .icon--large');
+    let titles = document.querySelectorAll('h1:not(.home-title), h2, h3, h3 span, h4, h5');
+    let labels = document.querySelectorAll('label');
+
+    navbarMobile.classList.add('is_pro');
+    labels.forEach(label => {
+        label.style.color = adminColor;
+    })
+    titles.forEach(title => {
+        title.style.color = adminColor;
+    })
+    icons.forEach(icon => {
+        icon.classList.remove('is_active');
+        icon.classList.add('is_pro')
+    })
+    buttons.forEach(button => {
+        button.style.backgroundColor = adminColor;
+    })
+    links.forEach(link => {
+        link.style.color = adminColor;
+    })
+
+})();
