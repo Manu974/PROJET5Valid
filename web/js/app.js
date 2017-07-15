cf06ad9c6596881dd6e0638c6fe2ae6e75846855
Imac-marc <marc.arnoult@hotmail.fr>
2017-07-15 13:41:06 +0200

    modification pour avoir la page contact en ajax

--- web/js/app.js
+++ web/js/app.js
@@ -4,6 +4,7 @@
     let menuBurger = new MenuSlider('.icon.icon--burger', '.navbar-mobile-content', '305px');
 
     new HeaderShrink('.header', 200, '88px', menuBurger);
-    new loadLoginPage('#login-page', '/login', 'header .icon--connection, .home-container .icon--connection');
+    let loginPage = new loadAjaxPage('#login-page', '/login', 'header .icon--connection, .home-container .icon--connection', '.login-content');
+    let contactPage = new loadAjaxPage('#contact-page', '/programmes/contact', 'header .icon--contact', '.contact-content');
 })();
 
--- web/js/src/getPage.js
+++ web/js/src/getPage.js
@@ -1,46 +1,48 @@
-class loadLoginPage {
+class loadAjaxPage {
     /**
      *
      * @param element
      * @param page
+     * @param clickEvent
+     * @param elementOfTheloadedPage
      */
-    constructor (element, page, clickEvent) {
+    constructor (element, page, clickEvent, elementOfTheloadedPage ) {
         this.element = document.querySelector(element);
-        this.loginContent = null;
+        this.pageContent = null;
         this.page = page;
-        this.getPage();
+        this.getPage(elementOfTheloadedPage);
         let links = document.querySelectorAll(clickEvent);
         links.forEach((link) => {
             link.parentNode.addEventListener('click', e => {
                 e.stopPropagation();
-                this.loginContent.style.display = 'block';
+                this.pageContent.style.display = 'block';
                 this.element.style.display = 'block';
                 setTimeout(() => {
-                    this.loginContent.style.left = 0;
+                    this.pageContent.style.left = 0;
                 }, 90);
             });
         });
         document.body.addEventListener('click', e => {
             e.stopPropagation();
-            this.loginContent.style.left = '-1500px';
+            this.pageContent.style.left = '-1500px';
             setTimeout(() => {
                 this.element.style.display = 'none';
             }, 700);
             setTimeout(() => {
-                this.loginContent.style.display = 'none';
+                this.pageContent.style.display = 'none';
             }, 700)
         });
     }
 
-    getPage() {
+    getPage(elementOfTheloadedPage) {
         let instance = axios.create({
             headers:{'X-Requested-With': 'XMLHttpRequest'}
         });
         instance.get(this.page)
             .then(res => {
                 this.element.innerHTML = res.data;
-                this.loginContent = document.querySelector('.login-content');
-                this.loginContent.addEventListener('click', e => e.stopPropagation());
+                this.pageContent = document.querySelector(elementOfTheloadedPage);
+                this.pageContent.addEventListener('click', e => e.stopPropagation());
             })
     }
 }
