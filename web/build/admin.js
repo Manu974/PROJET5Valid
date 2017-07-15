8791ac7978c66a04eee881e55e33e62b595cb626
Imac-marc <marc.arnoult@hotmail.fr>
2017-07-15 20:52:18 +0200

    finalisation de la page mentions legales

--- web/build/admin.js
+++ web/build/admin.js
@@ -1,10 +1,159 @@
-!function(e){function o(n){if(t[n])return t[n].exports;var r=t[n]={i:n,l:!1,exports:{}};return e[n].call(r.exports,r,r.exports,o),r.l=!0,r.exports}var t={};o.m=e,o.c=t,o.i=function(e){return e},o.d=function(e,t,n){o.o(e,t)||Object.defineProperty(e,t,{configurable:!1,enumerable:!0,get:n})},o.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return o.d(t,"a",t),t},o.o=function(e,o){return Object.prototype.hasOwnProperty.call(e,o)},o.p="/build/",o(o.s=1)}({1:/*!*******************************************************************************!*\
-  !*** multi ./web/js/src/admin/admin-app.js ./web/js/src/admin/admin-pages.js ***!
-  \*******************************************************************************/
-function(e,o,t){t(/*! ./web/js/src/admin/admin-app.js */"rTUe"),e.exports=t(/*! ./web/js/src/admin/admin-pages.js */"rXmu")},rTUe:/*!***************************************!*\
-  !*** ./web/js/src/admin/admin-app.js ***!
-  \***************************************/
-function(e,o){!function(){document.querySelector(".header-logo").attributes.src.value="/logo-icones/logo-admin.svg";var e=document.querySelectorAll(".header-navbar-item");e.forEach(function(e){e.classList.add("is_pro");var o=e.childNodes[3],t=e.childNodes[1],n=window.getComputedStyle(o).getPropertyValue("color");e.addEventListener("mouseover",function(){o.style.color="#00A99D"}),e.addEventListener("mouseout",function(){t.classList.remove("is_pro"),o.style.color=n})}),document.querySelector(".icon.icon--burger").classList.add("is_pro"),document.querySelectorAll(".footer-item:not(.footer-item--center)").forEach(function(e){e.classList.add("is_pro");var o=e.childNodes[3],t=window.getComputedStyle(e).getPropertyValue("color");e.addEventListener("mouseover",function(){o.style.color="#00A99D"}),e.addEventListener("mouseout",function(){o.style.color=t})})}()},rXmu:/*!*****************************************!*\
-  !*** ./web/js/src/admin/admin-pages.js ***!
-  \*****************************************/
-function(e,o){!function(){var e=document.querySelector(".navbar-mobile-content"),o=document.querySelectorAll("section a:not(.btn), .article-footer a, .newsletter a"),t=document.querySelectorAll("section .btn:not(.btn--large), button[type=submit], .newsletter .snippet input[type=submit].btn"),n=document.querySelectorAll(".navbar-mobile-content .icon, section .icon, section:not(.carte-container) .icon--large, .comments .icon, .blog-title .icon, .observation-container .icon, .observation-container .icon--large"),r=document.querySelectorAll("h1:not(.home-title), h2, h3, h3 span, h4, h5"),c=document.querySelectorAll("label");e.classList.add("is_pro"),c.forEach(function(e){e.style.color="#00A99D"}),r.forEach(function(e){e.style.color="#00A99D"}),n.forEach(function(e){e.classList.remove("is_active"),e.classList.add("is_pro")}),t.forEach(function(e){e.style.backgroundColor="#00A99D"}),o.forEach(function(e){e.style.color="#00A99D"})}()}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/build/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./web/js/src/admin/admin-app.js":
+/***/ (function(module, exports) {
+
+(function () {
+
+    var headerLogo = document.querySelector('.header-logo');
+    headerLogo.attributes.src.value = '/logo-icones/logo-admin.svg';
+
+    var headerItems = document.querySelectorAll('.header-navbar-item');
+    var colorAdmin = '#00A99D';
+
+    headerItems.forEach(function (item) {
+        item.classList.add('is_pro');
+        var link = item.childNodes[3];
+        var icon = item.childNodes[1];
+        var colorLink = window.getComputedStyle(link).getPropertyValue('color');
+
+        item.addEventListener('mouseover', function () {
+            link.style.color = colorAdmin;
+        });
+        item.addEventListener('mouseout', function () {
+            icon.classList.remove('is_pro');
+            link.style.color = colorLink;
+        });
+    });
+    var menuBurger = document.querySelector('.icon.icon--burger');
+    menuBurger.classList.add('is_pro');
+
+    var footerItems = document.querySelectorAll('.footer-item:not(.footer-item--center)');
+    footerItems.forEach(function (item) {
+        item.classList.add('is_pro');
+        var link = item.childNodes[3];
+        var colorLink = window.getComputedStyle(item).getPropertyValue('color');
+
+        item.addEventListener('mouseover', function () {
+            link.style.color = colorAdmin;
+        });
+        item.addEventListener('mouseout', function () {
+            link.style.color = colorLink;
+        });
+    });
+})();
+
+/***/ }),
+
+/***/ "./web/js/src/admin/admin-pages.js":
+/***/ (function(module, exports) {
+
+(function () {
+    var adminColor = '#00A99D';
+
+    var navbarMobile = document.querySelector('.navbar-mobile-content');
+    var links = document.querySelectorAll('section a:not(.btn), .article-footer a, .newsletter a');
+    var buttons = document.querySelectorAll('section .btn:not(.btn--large), button[type=submit], .newsletter .snippet input[type=submit].btn');
+    var icons = document.querySelectorAll('.navbar-mobile-content .icon, section .icon, section:not(.carte-container) .icon--large, .comments .icon, .blog-title .icon, .observation-container .icon, .observation-container .icon--large');
+    var titles = document.querySelectorAll('h1:not(.home-title), h2, h3, h3 span, h4, h5');
+    var labels = document.querySelectorAll('label');
+
+    navbarMobile.classList.add('is_pro');
+    labels.forEach(function (label) {
+        label.style.color = adminColor;
+    });
+    titles.forEach(function (title) {
+        title.style.color = adminColor;
+    });
+    icons.forEach(function (icon) {
+        icon.classList.remove('is_active');
+        icon.classList.add('is_pro');
+    });
+    buttons.forEach(function (button) {
+        button.style.backgroundColor = adminColor;
+    });
+    links.forEach(function (link) {
+        link.style.color = adminColor;
+    });
+})();
+
+/***/ }),
+
+/***/ 1:
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__("./web/js/src/admin/admin-app.js");
+module.exports = __webpack_require__("./web/js/src/admin/admin-pages.js");
+
+
+/***/ })
+
+/******/ });
