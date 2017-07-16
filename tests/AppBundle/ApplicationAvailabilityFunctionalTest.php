5a01968905b29d6c786796ac086c1d49c60f3368
Imac-marc <marc.arnoult@hotmail.fr>
2017-07-16 12:16:55 +0200

    ajout des tests pour le role admin

--- tests/AppBundle/ApplicationAvailabilityFunctionalTest.php
+++ tests/AppBundle/ApplicationAvailabilityFunctionalTest.php
@@ -5,13 +5,23 @@ namespace Tests\AppBundle;
 
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class ApplicationAvailabilityFunctionalTest extends WebTestCase
 {
+    private $client = null;
+
+    public function setUp()
+    {
+        $this->client = static::createClient();
+    }
+
     /**
-     * @dataProvider urlProvider
+     * @dataProvider urlProviderAnonymous
      */
-    public function testPageIsSuccessful($url)
+    public function testUrlForAnonymous($url)
     {
         $client = self::createClient();
         $client->request('GET', $url);
@@ -19,17 +29,54 @@ class ApplicationAvailabilityFunctionalTest extends WebTestCase
         $this->assertTrue($client->getResponse()->isSuccessful());
     }
 
-    public function urlProvider()
+    /**
+     * @dataProvider urlProviderAdmin
+     */
+    public function testUrlForAdmin($url)
+    {
+        $this->logIn();
+        $this->client->request('GET', $url);
+
+        $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+    }
+
+    // Authenticate an user with the role admin.
+    private function logIn()
+    {
+        $session = $this->client->getContainer()->get('session');
+
+        // the firewall context defaults to the firewall name
+        $firewallContext = 'main';
+
+        $token = new UsernamePasswordToken('admin', null, $firewallContext, array('ROLE_ADMIN'));
+        $session->set('_security_'.$firewallContext, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+    }
+
+    public function urlProviderAnonymous()
     {
         return array(
             array('/'),
-            array('/observation'),
             array('/faq'),
             array('/programmes'),
             array('/programmes/contact'),
-            array('/observation/carte'),
             array('/login'),
-            array('/newsletter')
+            array('/newsletter/'),
+        );
+    }
+
+    public function urlProviderAdmin()
+    {
+        return array(
+            array('/observation'),
+            array('/observation/carte'),
+            array('/observation/espacepro'),
+            array('/faq/admin'),
+            array('/programmes/admin'),
+            array('/programmes/admin/newblog'),
         );
     }
 }
